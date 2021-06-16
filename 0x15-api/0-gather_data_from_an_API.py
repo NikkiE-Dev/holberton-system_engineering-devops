9b7189ce4183b4204b0c13c3c7bcb3090b044cc2
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-16 20:08:23 +0000

    Adding file gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+
+def employ_task(employeeID):
+    name = ''
+    task_list = []
+    compl_task = 0
+
+    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                         .format(employeeID))
+    todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos/'
+                         .format(employeeID))
+
+    # Get the JSON from responses
+    name = users.json().get('name')
+    todosJson = todos.json()
+
+    # loop the task
+    for task in todosJson:
+        if task.get('completed') is True:
+            compl_task += 1
+            task_list.append(task.get('title'))
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, compl_task, len(todosJson)))
+
+    # Loop the task_list and print tasks
+    for title in task_list:
+        print("\t {}".format(title))
+
+    return 0
+
+if __name__ == "__main__":
+    employ_task(sys.argv[1])
