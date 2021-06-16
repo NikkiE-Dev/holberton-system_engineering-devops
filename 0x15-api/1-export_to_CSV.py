3bc01262f66345aeffe30f20bc818c291587c243
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-16 20:09:06 +0000

    Adding export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This is for API"""
+import csv
+import requests
+import sys
+
+
+def save_to_csv(employeeID):
+    username = ''
+    allTasks = []
+
+    users = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employeeID))
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos/'.format(employeeID))
+
+    username = users.json().get('username')
+    todosJson = todos.json()
+
+    for task in todosJson:
+        taskRow = []
+        taskRow.append(employeeID)
+        taskRow.append(username)
+        taskRow.append(task.get("completed"))
+        taskRow.append(task.get("title"))
+
+        allTasks.append(taskRow)
+
+    with open("{}.csv".format(employeeID), 'w') as csvFile:
+        csvwriter = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
+        csvwriter.writerows(allTasks)
+    return 0
+
+if __name__ == "__main__":
+    save_to_csv(sys.argv[1])
